e69671a63ab22ab1d92ca3c9ac1cc1a2f99953c5
uhammadziyo <DilafruzIminova71@gmail.com>
2024-08-27 08:55:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+let a = 3
+
+let b  = 6
+
+console.log(a + b);
+
+
+
+let d = Number('12');
+
+
+ console.log(typeof a);
+
+
+
+let random = Math.floor(Math.random()*100)
+
+if(random % 2 == 0){
+    console.log();
+} 
